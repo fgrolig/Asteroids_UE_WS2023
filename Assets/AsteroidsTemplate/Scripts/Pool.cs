a3582f2c3c6ef4a73f4ee1a228f14477ae2cae8a
Benjamin Rose <br.collecting@gmail.com>
2023-11-06 18:01:48 +0100

    Pools can now spawn from a random pool of objects, can reference a prefab instead of in-scene object

--- Assets/AsteroidsTemplate/Scripts/Pool.cs
+++ Assets/AsteroidsTemplate/Scripts/Pool.cs
@@ -6,6 +6,10 @@ public class Pool : MonoBehaviour
 	[SerializeField]
 	private GameObject templateObject = default;
 
+	[Tooltip("Use a random object from the randomizedObjectPool instead of the template object?")]
+	[SerializeField] private bool useRandomizedObjectPoolInstead = false;
+	[SerializeField] private List<GameObject> randomizedObjectPool = new List<GameObject>();
+
 	[SerializeField]
 	private int numberOfPregenerated = 0;
 
@@ -13,8 +17,7 @@ public class Pool : MonoBehaviour
 
 	private void Awake()
 	{
-		templateObject.SetActive(false);
-
+		if(templateObject != null && templateObject.gameObject.scene.name != null) templateObject.SetActive(false);
 		objects = new List<GameObject>();
 
 		GameObject tmpObject;
@@ -37,16 +40,34 @@ public class Pool : MonoBehaviour
 		}
 
 		GameObject newObject = CreateObject();
+		newObject.SetActive(true);
 
 		return newObject;
 	}
 
 	private GameObject CreateObject()
 	{
-		GameObject newObject = Instantiate(templateObject);
-		newObject.transform.parent = templateObject.transform.parent;
+		GameObject newObject = null;
+		if (useRandomizedObjectPoolInstead)
+		{
+			if (randomizedObjectPool.Count == 0)
+			{
+				Debug.LogError("No randomized Object Pool has been set!", this);
+			}
+			
+			newObject = Instantiate(randomizedObjectPool[Random.Range(0, randomizedObjectPool.Count)]);
+		}
+		else
+		{
+			newObject = Instantiate(templateObject);
+
+		}
+		
+		newObject.transform.parent = transform;
 
 		objects.Add(newObject);
+		
+		newObject.SetActive(false);
 
 		return newObject;
 	}
